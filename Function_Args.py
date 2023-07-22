708ecc8bd50529366c5a9b13fba45e8a39804df4
Barasa-Zetech <123546945+Barasa-Zetech@users.noreply.github.com>
2023-07-22 15:19:28 +0300

    Add files via upload

---
+++ Function_Args.py
@@ -0,0 +1,16 @@
+### Method_3
+######Function Python program without argument; i.e Default Parameter Value:-
+
+def Kenya(County = "Mombasa"):
+
+    print("I am from " + County)
+
+Kenya()
+
+Kenya("Nairobi")
+
+Kenya("Kiambu")
+
+Kenya("Kisumu")
+
+Kenya("Kakamega")
